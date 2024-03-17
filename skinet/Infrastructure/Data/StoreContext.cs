05306f8064e4c00baaaf57afa6ba0b4ae71ff501
hamid haghverdi <hamidhaghverdi2002@gmail.com>
2024-03-17 01:05:52 -0700

    Projects created

---
+++ skinet/Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
