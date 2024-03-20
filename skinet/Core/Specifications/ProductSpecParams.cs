0308f013e9eb42745dc2141f15a163eada238ce6
hamid haghverdi <hamidhaghverdi2002@gmail.com>
2024-03-20 04:54:55 -0700

    Search and sort bug fixed

--- skinet/Core/Specifications/ProductSpecParams.cs
+++ skinet/Core/Specifications/ProductSpecParams.cs
@@ -20,9 +20,9 @@ namespace Core.Specifications
 
         public int? BrandId { get; set; }
         public int? TypeId { get; set; }
-        public string Sort { get; set; }
+        public string? Sort { get; set; }
         private string _search;
-        public string Search
+        public string? Search
         {
             get => _search;
             set => _search = value.ToLower();
