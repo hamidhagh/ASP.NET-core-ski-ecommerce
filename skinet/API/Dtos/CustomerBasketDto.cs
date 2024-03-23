a1261b9404ff0c9e26df7dea9f69016b3ac1974e
hamid haghverdi <hamidhaghverdi2002@gmail.com>
2024-03-23 04:08:55 -0700

    Validation implemented

---
+++ skinet/API/Dtos/CustomerBasketDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Dtos
+{
+    public class CustomerBasketDto
+    {
+        [Required]
+        public string Id { get; set; }
+        public List<BasketItemDto> Items { get; set; }
+    }
+}
--- skinet/API/Program.cs
+++ skinet/API/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddSwaggerDocumentation();
 
 
 var app = builder.Build();
@@ -23,12 +24,12 @@ app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwaggerDocumentation();
 //app.UseHttpsRedirection();
 
 app.UseStaticFiles();
